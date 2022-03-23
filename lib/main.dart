bb08dacd15824cea139ea74dc49e41d5a43e698d
braulio <bviveros@mail.csuchico.edu>
2022-03-22 23:11:43 -0700

    essential changes to making app work

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:pingme/authentication/login.dart';
@@ -62,8 +61,6 @@ class _MyAppState extends State<MyApp> {
         home: StartupLogic().getLandingPage(context));
   }
 }
-
-
 
 // DUMMY HOME PAGE
 class SuccessPage extends StatefulWidget {
@@ -142,46 +139,3 @@ class _SuccessPage extends State<SuccessPage> {
         )));
   }
 }
-=======
-import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-
-void main() => runApp(const MyApp());
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  late GoogleMapController mapController;
-
-  final LatLng _center = const LatLng(45.521563, -122.677433);
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Ping-Me'),
-          backgroundColor: Colors.lightBlue,
-          centerTitle: true,
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-        ),
-      ),
-    );
-  }
-}
->>>>>>> map
