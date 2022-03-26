3ded8480f6793c15d7d215d13b4dc2f089e6b399
Cole-Hop <chopkins1@mail.csuchico.edu>
2022-03-25 18:51:25 -0700

    Initial Locate Feature

--- lib/home.dart
+++ lib/home.dart
@@ -4,22 +4,21 @@ import 'package:location/location.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
-
   @override
-  _HomeState createState() => _HomeState();
+  HomeState createState() => HomeState(); //init class HomeState
 }
 
-class _HomeState extends State<HomePage> {
-  late GoogleMapController mapController;
-  LatLng initcamposition = LatLng(45.521563, -122.677433);
-  Location location = Location();
+class HomeState extends State<HomePage> {
+  late GoogleMapController mapController; //load google apps function from google_maps_flutter plugin
+  LatLng initcamposition = const LatLng(45.521563, -122.677433); //default cam position
+  Location location = Location(); //enable location tracking from user device using location plugin
 
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-    location.onLocationChanged.listen((l) {
-      mapController.animateCamera(
-        CameraUpdate.newCameraPosition(
-          CameraPosition(target: LatLng(l.latitude!, l.longitude!),zoom: 16),
+  void _onMapCreated(GoogleMapController controller) { //create map
+    mapController = controller; //allow for looking around map
+    location.onLocationChanged.listen((l) { //listen to user current position
+      mapController.animateCamera( //lock onto user position
+        CameraUpdate.newCameraPosition( //update if user position changes
+          CameraPosition(target: LatLng(l.latitude!, l.longitude!),zoom: 16), //fetch new position
         ),
       );
     });
@@ -30,37 +29,37 @@ class _HomeState extends State<HomePage> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-            title: const Text('Ping-Me'),
+            title: const Text('Ping-Me'), //title header
             backgroundColor: Colors.blue,
             centerTitle: true,
-            leading: IconButton(
+            leading: IconButton( //settings button
             icon: const Icon(Icons.settings),
               onPressed: () {},
-            )),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: initcamposition,
-            zoom: 1.0,
-          ),
-          myLocationEnabled: true,
+            )
         ),
-        bottomNavigationBar: BottomAppBar(
-          shape: const CircularNotchedRectangle(),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated, //build map
+          initialCameraPosition: CameraPosition(
+            target: initcamposition, //initial position
+            zoom: 1.0, //initial zoom (globe)
+          ),
+          myLocationEnabled: true, //allow for permission to track user
+        ),
+        bottomNavigationBar: BottomAppBar( //footer navigation bar
+          shape: const CircularNotchedRectangle(), //navigation bar layout
           notchMargin: 6.0,
           color: Colors.blue,
           child: Row(
             children: [
-              //IconButton(icon: Icon(Icons.menu), onPressed: () {}),
-              const Spacer(),
+              const Spacer(), //allow for friends icon to appear right side
               IconButton(
-                  icon: const Icon(Icons.perm_identity_outlined,
+                  icon: const Icon(Icons.perm_identity_outlined, //friends button
                       color: Colors.white),
                   onPressed: () {}),
             ],
           ),
         ),
-        floatingActionButton: FloatingActionButton(
+        floatingActionButton: FloatingActionButton( //map button
             child: const Icon(Icons.public), onPressed: () {}),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       ),
