cc7b35a6e46f05bbc46edc5be85346a6a4d28372
braulio <bviveros@mail.csuchico.edu>
2022-03-28 21:13:20 -0700

    successfully added all firestore dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'authentication/firebase_options.dart';
 import 'package:pingme/authentication/login.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 // CSCI 430: pingme
-// Names: Jorge Munoz,
+// Names: Jorge Munoz, Crispin , Cole Hopkins, Braulio Viveros
 
 // ~TODO(mvp): Get a home page with google maps intigration.
 // ~TODO: Bottom menu bar to navigate between pages
