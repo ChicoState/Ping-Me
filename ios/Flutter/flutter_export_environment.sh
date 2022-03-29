db9055fd1c123e88312829b7c28e9ab8975493bd
braulio <bviveros@mail.csuchico.edu>
2022-03-28 22:38:16 -0700

    resolved merge conflicts with iOS flutter export environments.sh

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -3,19 +3,11 @@
 export "FLUTTER_ROOT=/Library/Developer/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/brau/SE/Ping-Me"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-<<<<<<< HEAD
 export "FLUTTER_TARGET=/Users/brau/SE/Ping-Me/lib/main.dart"
-=======
-export "FLUTTER_TARGET=lib/main.dart"
->>>>>>> 1840e8072386d2be526f20a5e7424ca51a8ad817
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
-<<<<<<< HEAD
 export "PACKAGE_CONFIG=/Users/brau/SE/Ping-Me/.dart_tool/package_config.json"
-=======
-export "PACKAGE_CONFIG=.packages"
->>>>>>> 1840e8072386d2be526f20a5e7424ca51a8ad817
