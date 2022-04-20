b1d63423afcac8c2fbe4f6829e0d9d4929aeed53
Jorge Munoz <jmunoz2414995@gmail.com>
2022-04-19 17:10:10 -0700

    location upload

--- lib/home.dart
+++ lib/home.dart
@@ -5,6 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:pingme/authentication/login.dart';
 import 'package:location/location.dart' hide LocationAccuracy;
 import 'package:geolocator/geolocator.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -18,16 +19,26 @@ Future<Position> _getGeoLocationPosition() async {
 
 class HomeState extends State<HomePage> {
   late Position _currentPosition;
-  late GoogleMapController mapController; //load google apps function from google_maps_flutter plugin
-  LatLng initcamposition = const LatLng(45.521563, -122.677433); //default cam position
-  Location location = Location(); //enable location tracking from user device using location plugin
+  late GoogleMapController
+      mapController; //load google apps function from google_maps_flutter plugin
+  LatLng initcamposition =
+      const LatLng(45.521563, -122.677433); //default cam position
+  Location location =
+      Location(); //enable location tracking from user device using location plugin
+  final firestoreInstance = FirebaseFirestore.instance;
 
-  void _onMapCreated(GoogleMapController controller) { //create map
+  void _onMapCreated(GoogleMapController controller) {
+    //create map
     mapController = controller; //allow for looking around map
-    location.onLocationChanged.listen((l) { //listen to user current position
-      mapController.animateCamera( //lock onto user position
-        CameraUpdate.newCameraPosition( //update if user position changes
-          CameraPosition(target: LatLng(l.latitude!, l.longitude!),zoom: 16), //fetch new position
+    location.onLocationChanged.listen((l) {
+      //listen to user current position
+      mapController.animateCamera(
+        //lock onto user position
+        CameraUpdate.newCameraPosition(
+          //update if user position changes
+          CameraPosition(
+              target: LatLng(l.latitude!, l.longitude!),
+              zoom: 16), //fetch new position
         ),
       );
     });
@@ -41,11 +52,11 @@ class HomeState extends State<HomePage> {
             title: const Text('PingMe'),
             backgroundColor: Colors.blue,
             centerTitle: true,
-            leading: IconButton( //settings button
+            leading: IconButton(
+              //settings button
               icon: const Icon(Icons.settings),
               onPressed: () {},
-            )
-        ),
+            )),
         body: GoogleMap(
           onMapCreated: _onMapCreated, //build map
           initialCameraPosition: CameraPosition(
@@ -54,7 +65,8 @@ class HomeState extends State<HomePage> {
           ),
           myLocationEnabled: true, //allow for permission to track user
         ),
-        bottomNavigationBar: BottomAppBar( //footer navigation bar
+        bottomNavigationBar: BottomAppBar(
+          //footer navigation bar
           shape: const CircularNotchedRectangle(), //navigation bar layout
           notchMargin: 6.0,
           color: Colors.blue,
@@ -75,8 +87,9 @@ class HomeState extends State<HomePage> {
               const Spacer(), //allow for friends icon to appear right side
 
               IconButton(
-                  icon: const Icon(Icons.perm_identity_outlined, //friends button
-                      color: Colors.white),
+                  icon:
+                      const Icon(Icons.perm_identity_outlined, //friends button
+                          color: Colors.white),
                   onPressed: () {
                     Navigator.push(
                         context,
@@ -86,21 +99,34 @@ class HomeState extends State<HomePage> {
             ],
           ),
         ),
-        floatingActionButton: FloatingActionButton( //map button
-            child: const Icon(Icons.public), onPressed: () async {
-          Position geoPosition = await _getGeoLocationPosition();
-          showDialog(
-              context: context,
-              builder: (context) => AlertDialog(
-                  title: const Text('Current Location'),
-                  content: Text("LAT: ${geoPosition.latitude}, LNG: ${geoPosition.longitude}"),
-                  actions: [
-                    TextButton(
-                      child: const Text('OK'),
-                      onPressed: () => Navigator.pop(context),
-                    )
-                  ]));
-        }),
+        floatingActionButton: FloatingActionButton(
+            //map button
+            child: const Icon(Icons.public),
+            onPressed: () async {
+              Position geoPosition = await _getGeoLocationPosition();
+              var firebaseUser = FirebaseAuth.instance.currentUser;
+              if (firebaseUser != null) {
+                await firestoreInstance
+                    .collection("userEmails")
+                    .doc(firebaseUser.uid)
+                    .update({
+                  'location':
+                      GeoPoint(geoPosition.latitude, geoPosition.longitude)
+                });
+              }
+              showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                          title: const Text('Current Location'),
+                          content: Text(
+                              "LAT: ${geoPosition.latitude}, LNG: ${geoPosition.longitude}"),
+                          actions: [
+                            TextButton(
+                              child: const Text('OK'),
+                              onPressed: () => Navigator.pop(context),
+                            )
+                          ]));
+            }),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       ),
     );
